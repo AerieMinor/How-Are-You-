e6e9d7db5b2f2c0b82c08fce3770e70a8d893671
Aerie Minor <39811248+AerieMinor@users.noreply.github.com>
2019-04-16 23:10:04 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname=" ", lastname;
+            string[] arrayG = { "great", "good", "alright", "horrible" };
+            string quality = arrayG[new Random().Next(0,arrayG.Length)];
+
+            while (firstname != "end")
+            { 
+
+                Console.WriteLine("Please enter your first name: ");
+                firstname = Console.ReadLine();
+                Console.WriteLine("Please enter your last name: ");
+                lastname = Console.ReadLine();
+                Console.WriteLine("{0} {1}, you are" + " " + quality +" " + "!", firstname, lastname);
+                Console.ReadLine();
+            }
+        }
+    }
+}
